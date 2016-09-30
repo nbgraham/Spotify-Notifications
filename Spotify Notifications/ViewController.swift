59eef0f94c32c2b9468d5efa8ecdcfec36cfea29
Nick Graham <nbgraham@user.noreply.github.com>
2016-09-30 13:59:20 -0500

    Shared schema
    Trying to get CI working

--- Spotify Notifications/ViewController.swift
+++ Spotify Notifications/ViewController.swift
@@ -10,63 +10,70 @@ import Cocoa
 import NSBundle_LoginItem
 
 class ViewController: NSViewController {
-
-    /*
-    Button              Preferency Key              Purpose
-    -----------------------------------------------------------------------------------------
-    notificationSound   playSoundOnNotification     Play a sound before each notification.
-    embedAlbumArtwork   embedAlbumArtwork           Embed the album artwork in notifications.
-    spotifyHasFocus     disableWhenSpotifyHasFocus  Disable notifications when the Spotify
-                                                    has focus
-    launchOnLogin       n/a                         Automatically launch the application on
-                                                    login
     
-    */
-
-    @IBOutlet weak var notificationSoundButton: NSPopUpButton!
-    @IBOutlet weak var embedAlbumArtworkButton: NSPopUpButton!
-    @IBOutlet weak var spotifyHasFocusButton: NSPopUpButton!
-    @IBOutlet weak var launchOnLoginButton: NSPopUpButton!
-
+    /*
+     Button              Preferency Key              Purpose
+     -----------------------------------------------------------------------------------------
+     notificationSound   playSoundOnNotification     Play a sound before each notification.
+     embedAlbumArtwork   embedAlbumArtwork           Embed the album artwork in notifications.
+     spotifyHasFocus     disableWhenSpotifyHasFocus  Disable notifications when the Spotify
+     has focus
+     launchOnLogin       n/a                         Automatically launch the application on
+     login
+     
+     */
+    
+    @IBOutlet weak var notificationSoundCheckBox: NSButton!
+    @IBOutlet weak var embedAlbumArtworkCheckBox: NSButton!
+    @IBOutlet weak var hideSpotifyHasFocusCheckBox: NSButton!
+    @IBOutlet weak var launchOnLoginCheckBox: NSButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        notificationSoundButton.selectItem(at: fetchPreference("playSoundOnNotification"))
-        embedAlbumArtworkButton.selectItem(at: fetchPreference("embedAlbumArtwork"))
-        spotifyHasFocusButton.selectItem(at: fetchPreference("disableWhenSpotifyHasFocus"))
         
-        if Bundle.main.isLoginItemEnabled() {
-            launchOnLoginButton.selectItem(at: 0)
-        } else {
-            launchOnLoginButton.selectItem(at: 1)
+        if notificationSoundCheckBox.state == 0 {
+            notificationSoundCheckBox.setNextState()
+        }
+        
+        if embedAlbumArtworkCheckBox.state == 0 {
+            embedAlbumArtworkCheckBox.setNextState()
+        }
+        
+        if hideSpotifyHasFocusCheckBox.state == 0 {
+            hideSpotifyHasFocusCheckBox.setNextState()
+        }
+        
+        let defaultState = Bundle.main.isLoginItemEnabled() ? 1 : 0
+        if launchOnLoginCheckBox.state != defaultState {
+            launchOnLoginCheckBox.setNextState()
         }
     }
-
+    
     override var representedObject: Any? {
         didSet {
-        // Update the view, if already loaded.
+            // Update the view, if already loaded.
         }
     }
-
-    @IBAction func PreferenceSet(_ sender: NSPopUpButton) {
+    
+    @IBAction func PreferenceSet(_ sender: NSButton) {
         let identifier : String = sender.identifier!
         
         if identifier == "launchOnLogin" {
-            if sender.indexOfSelectedItem == 0 {
+            if sender.state == 0 {
                 Bundle.main.enableLoginItem()
             } else {
                 Bundle.main.disableLoginItem()
             }
         } else {
-            setPreference(identifier, value: sender.indexOfSelectedItem)
+            setPreference(identifier, value: sender.state)
         }
     }
-
+    
     func setPreference(_ key: String, value: Int) {
         UserDefaults.standard.set(value, forKey: key)
         UserDefaults.standard.synchronize()
     }
-
+    
     func fetchPreference(_ key: String, fallback: Int = 0) -> Int {
         if let preference: AnyObject = UserDefaults.standard.object(forKey: key) as AnyObject? {
             return preference as! Int
@@ -74,6 +81,6 @@ class ViewController: NSViewController {
             return fallback
         }
     }
-
+    
 }
 
